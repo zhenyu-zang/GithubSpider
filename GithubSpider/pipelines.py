3e18c56dd40043b1fd838adaa8431da87c2aadae
FrankZn <lygzzy@gmail.com>
2019-03-14 15:01:06 +0800

    Add mysql pipeline.

--- GithubSpider/pipelines.py
+++ GithubSpider/pipelines.py
@@ -5,7 +5,55 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
+import json
+import mysql.connector
 
 class GithubspiderPipeline(object):
     def process_item(self, item, spider):
+        if item.get('owner_name'):
+            return item
+        else:
+            raise DropItem('Missing owner name in {}'.format(item))
+
+class MySQLPipeline(object):
+    def __init__(self):
+        with open('secret.json', 'r') as jsonfile:
+            self.mysql_conn_info = json.load(jsonfile)
+            
+
+    def open_spider(self, spider):
+        self.db = mysql.connector.connect(**self.mysql_conn_info)
+        self.cursor = db.cursor()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.db.close()
+
+    def process_item(self, item, spider):
+        # Insert repo
+        insert_repo = ("INSERT INTO repos "
+               "(repo_name, owner_name, git_url) "
+               "VALUES (%s, %s, %s)")
+        repo_data = (item['name'], item['owner_name'], item['git_url'])
+        self.cursor.execute(insert_repo, repo_data)
+        repo_no = cursor.lastrowid
+
+        # Insert tags
+        query_tag = ("SELECT tag_no from tags "
+            "WHERE name=%s")
+        insert_tag = ("INSERT INTO TAGS "
+            "(tag_name) VALUES (%s)")
+        insert_repo_tag = ("INSERT INTO repo_tag "
+            "(repo_no, tag_no) VALUES (%s, %s)")
+        for tag in item['tags']:
+            cursor.execute(query_tag, (tag))
+            (tag_no) = cursor.fetchone()
+            if tag_no is None:
+                cursor.execute(insert_tag, (tag))
+                tag_no = cursor.lastrowid
+            cursor.execute(insert_repo_tag, (repo_no, tag_no))
+
+
+
         return item
--- GithubSpider/settings.py
+++ GithubSpider/settings.py
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'GithubSpider.pipelines.GithubspiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'GithubSpider.pipelines.GithubspiderPipeline': 300,
+   'GithubSpider.pipelines.MySQLPipeline': 400
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
